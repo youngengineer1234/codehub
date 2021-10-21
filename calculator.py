d310a2680439a28943320e2f2a9e2cf3b744d5a3
youngengineer1234 <theyoungengineer.com@gmail.com>
2021-10-21 11:06:21 +0530

    update the calculaor, create the selected file

--- calculator.py
+++ calculator.py
@@ -1,1 +1,20 @@
 #create a calculator
+
+operand_1 = int(input("Enter operand_1"))
+operand_2 = int(input("Enter operand_1"))
+
+operator = input("Enter any operator i.e. '+'/'-'/'*'/'/'")
+
+if(operator == "+"):
+    result = operand_1 + operand_2
+elif(operator == "-"):
+    result = operand_1 - operand_2
+elif(operator == "*"):
+    result = operand_1 * operand_2
+elif(operator == "/"):
+    result = operand_1 / operand_2
+else:
+    result = 0
+print(result)
+print("Thank You")
+
---
+++ selected.py
@@ -0,0 +1,1 @@
+#create a program whether an individual is able to vaotre or not.
