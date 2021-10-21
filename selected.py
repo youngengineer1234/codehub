d503d6a2655cf141b1f486bfb9b077830d3290b7
youngengineer1234 <theyoungengineer.com@gmail.com>
2021-10-21 11:47:35 +0530

    finish selected and create even file

--- selected.py
+++ selected.py
@@ -1,1 +1,8 @@
 #create a program whether an individual is able to vaotre or not.
+age = int(input("Enter your age: "))
+
+if(age >= 18):
+    print("Congrats! You are eligible to vote")
+else:
+    print("Ooops! YOu are not eligible.")
+print("Thank You for the survey!!")
